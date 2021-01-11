687ac3bace7cfee66189f4f225ba2e91c49b5f74
ykdc <73417092+ykdc@users.noreply.github.com>
2021-01-11 11:19:21 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+// Hello World program in C# for MLH LHD 2021
+namespace MyApp
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! It's Me!");
+        }
+    }
+}
